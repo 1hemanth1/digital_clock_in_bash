73bd3fb62341eb65a49324dade6a6972e0df9edb
Hemanth <hemanth.ch37@gmail.com>
2024-09-29 00:02:18 -0400

    Update digital_clock.sh

--- digital_clock.sh
+++ digital_clock.sh
@@ -1,10 +1,18 @@
-Green=$'\e[1;31m'
-Red=$'\e[1;32m'
-Blue=$'\e[1;34m'
+Red=$'\e[1;31m'
+Bold=$'\e[1m'
+Blink=$'\e[5m'
+Reset=$'\e[0m'
 
 while true
 do
         clear
-        echo $(date +%T)
-        sleep 1s
+         date=$(date +"%A, %d %B %Y")
+         time=$(date +"%H ${Blink}:${Reset} %M ${Blink}:${Reset} %S")
+
+ echo "${Red}=============================" 
+        echo "${Red}     $date     ${Reset}"
+        echo "${Red}${Bold}       $time       ${Reset}"
+ echo "${Red}============================="
+
+ sleep 1
 done
